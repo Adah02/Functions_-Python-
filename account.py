cd680252f3f02676b12a36b07596b657be3a3bec
Emmanuel Adah Edogbanya <edogbanya02@gmail.com>
2025-05-24 19:10:38 +0100

    Account testing in python

--- account.py
+++ account.py
@@ -1,3 +1,20 @@
+def set_account_name(set_name):
+  account_name = set_name
+  return account_name
+
+def set_account_number(set_acc_number):
+  set_number = str(set_acc_number)
+  if set_number.isdigit():
+    number_length = len(set_number)
+    if number_length == 10: 
+      account_number = set_acc_number
+      return account_number
+    else:
+      return "invalid"
+  else:
+    return "invalid"
+
+  
 def deposit_funds(balance, deposit_amount):
   if deposit_amount > 0:
     balance += deposit_amount
@@ -14,14 +31,17 @@ def withdraw_funds(balance, withdraw_amount):
   return balance
 
 
-
 balance = 5000;
 deposit_amount = 500;
 transfer_amount = 1500
 withdraw_amount = 2400
-print('Your balance after deposit is ',deposit_funds(balance, deposit_amount))
-print('Your balance after transfer is ',transfer_funds(balance, transfer_amount))
-print('Your balance after withdrawal is ',withdraw_funds(balance, withdraw_amount))
+set_name  = "Emma Adah"
+set_acc_number = 8160509785
+print('Your account name is',set_account_name(set_name))
+print('Your account number is',set_account_number(set_acc_number))
+print('Your balance after deposit is',deposit_funds(balance, deposit_amount))
+print('Your balance after transfer is',transfer_funds(balance, transfer_amount))
+print('Your balance after withdrawal is',withdraw_funds(balance, withdraw_amount))
 
 
 
--- test_account.py
+++ test_account.py
@@ -4,6 +4,17 @@ import account
 
 class TestAccount(TestCase):
 
+  def test_set_new_account_name(self):
+    new_name = "James Tauri"
+    check = account.set_account_name(new_name)
+    expected = "James Tauri"
+    self.assertEqual(check, expected)
+
+  def test_set_new_account_number(self):
+    check = account.set_account_number(2553627162)
+    expected = 2553627162;
+    self.assertEqual(check, expected)
+
   def test_make_deposit_to_account(self):
     check = account.deposit_funds(200, 500)
     expected = 700;
